62d75c393719ec03ce9eee16fafaff4cfb182fbb
Anikesh <anikeshpatel4@gmail.com>
2018-01-21 10:07:20 +0530

    exit functionality

--- app/src/main/java/anongrp/canny_thetic_tac_toegame/HomeActivity.java
+++ app/src/main/java/anongrp/canny_thetic_tac_toegame/HomeActivity.java
@@ -1,14 +1,26 @@
 package anongrp.canny_thetic_tac_toegame;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+    Button playBtn;
+    Button opetionBtn;
+    Button exitBtn;
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
+
+        playBtn = findViewById(R.id.playBtn);
+        opetionBtn = findViewById(R.id.optionBtn);
+        exitBtn = findViewById(R.id.exitBtn);
+    }
+
+    public void quit(View view) {
+        HomeActivity.this.finish();
     }
 }
